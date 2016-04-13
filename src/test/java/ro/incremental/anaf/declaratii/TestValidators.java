3890f05c86b132df4b0fa7c228b436d873ecc077
Alex Proca <alex.proca@gmail.com>
2016-04-13 09:37:15 +0300

    Added test

--- src/test/java/ro/incremental/anaf/declaratii/TestValidators.java
+++ src/test/java/ro/incremental/anaf/declaratii/TestValidators.java
@@ -6,6 +6,8 @@ import pdf.PdfCreation;
 
 import java.util.Set;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Alex Proca <alex.proca@gmail.com> on 26/03/16.
  */
@@ -14,7 +16,19 @@ public class TestValidators {
     @Test
     public void testValidators() throws Exception {
 
-        Reflections reflections = new Reflections("");
+        boolean containsDec106 = false;
+        for(Package p : Package.getPackages()) {
+
+            containsDec106 |= p.getName().contains("d106");
+
+        }
+
+        assertTrue("Anaf libraries are in test classpath", containsDec106);
+
+    }
+
+    private void listClassesInPackage(String packageName) {
+        Reflections reflections = new Reflections(packageName);
 
         Set<Class<? extends PdfCreation>> subTypes =
                 reflections.getSubTypesOf(PdfCreation.class);

f8f2b2e0766ea774b6d0c3b62c5aaf73f174b4bc
Alex Proca <alex.proca@gmail.com>
2016-04-13 23:21:01 +0300

    Generate pdf from json web server

--- src/main/java/ro/incremental/anaf/declaratii/ValidateWebService.java
+++ src/main/java/ro/incremental/anaf/declaratii/ValidateWebService.java
@@ -4,13 +4,52 @@ package ro.incremental.anaf.declaratii;
  * Created by Alex Proca <alex.proca@gmail.com> on 18/03/16.
  */
 
+import com.google.common.base.MoreObjects;
+import com.google.common.io.Files;
+import dec.Validation;
 import org.json.JSONObject;
+import pdf.PdfCreation;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/form")
 public class ValidateWebService {
+
+    private static final int PERIOADA_RAPORTARE_ERONATA = -4;
+    private static final int FISIER_VALID = 0;
+    private static final int TIP_DECLARATIE_NECUNOSCUT = -8;
+    public static final int UNKNOWN_ERROR = -9;
+
+    private static final Map<String, Class<PdfCreation>> creators = new HashMap<>();
+    private static final Map<String, Class<Validation>> validators = new HashMap<>();
+
+    static {
+        InputStream packages = Thread.currentThread().getContextClassLoader().getResourceAsStream("packages.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(packages));
+
+
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                try {
+
+                    creators.put(line, (Class<PdfCreation>) Class.forName(line + ".PdfCreator"));
+                    validators.put(line, (Class<Validation>) Class.forName(line + "validator.Validator"));
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @GET
     @Path("/available")
@@ -24,7 +63,117 @@ public class ValidateWebService {
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
     public String available(JSONObject input) {
+
+        String declName = input.getString("tagName").replace("declaratie", "d");
         String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + org.json.JSONML.toString(input);
-        return xml;
+
+        Result result = generateFromXMLString(xml, declName);
+
+        JSONObject ret = new JSONObject();
+
+        ret.append("message", result.message);
+        ret.append("pdfFilePath", result.pdfFilePath);
+        ret.append("resultCode", result.resultCode);
+
+        return ret.toString();
     }
+
+    public static class Result {
+        public String message;
+        public String pdfFilePath;
+        public int resultCode;
+
+        public Result(String message, String pdfFilePath, int resultCode) {
+            this.message = message;
+            this.pdfFilePath = pdfFilePath;
+            this.resultCode = resultCode;
+        }
+
+        @Override
+        public String toString() {
+            return MoreObjects.toStringHelper("Result")
+                    .add("Message", message)
+                    .add("PdfFilePath", pdfFilePath)
+                    .add("ResultCode", resultCode)
+                    .toString();
+        }
+    }
+
+    public static Result generateFromXMLString(String xml, String declName) {
+        try {
+            File xmlFile = File.createTempFile(declName, ".tmp");
+            Files.newWriter(xmlFile, Charset.forName("UTF-8")).append(xml).close();
+
+            return generateFromXMLFile(xmlFile.getAbsolutePath(), declName);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return new Result(e.getMessage(), "", UNKNOWN_ERROR);
+        }
+    }
+
+    public static Result generateFromXMLFile(String xmlFilePath, String declName) {
+        try {
+
+            Validation validator = validators.get(declName).newInstance();
+            PdfCreation pdfCreation = creators.get(declName).newInstance();
+
+            StringBuilder finalMessage = new StringBuilder();
+            String newLine = System.lineSeparator();
+
+            String errFilePath = xmlFilePath + ".err.txt";
+            String pdfFilePath = xmlFilePath + ".pdf";
+
+            int returnCode = validator.parseDocument(xmlFilePath, errFilePath);
+
+            switch (returnCode) {
+                case PERIOADA_RAPORTARE_ERONATA:
+                    finalMessage.append("Perioada raportare eronata: ").append(declName).append(newLine);
+                    break;
+                case TIP_DECLARATIE_NECUNOSCUT:
+                    finalMessage.append("Tip declaratie necunoscut: ").append(declName).append(newLine);
+                    break;
+                case FISIER_VALID:
+                    finalMessage.append("Validare fara erori fisier: ").append(xmlFilePath).append(newLine);
+                    break;
+                default:
+
+                    File errFile = new File(errFilePath);
+                    StringBuilder errorBuffer = new StringBuilder();
+                    if(errFile.exists()) {
+                        BufferedReader reader = Files.newReader(errFile, Charset.forName("UTF-8"));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            errorBuffer.append(line).append(newLine);
+                        }
+                    }
+
+                    if (returnCode > FISIER_VALID) {
+                        finalMessage.append("Atentionari la validare fisier: ").append(xmlFilePath).append(newLine);
+                        finalMessage.append(errorBuffer);
+                        break;
+                    }
+
+                    if (returnCode > PERIOADA_RAPORTARE_ERONATA) {
+                        finalMessage.append("Erori la validare fisier: ").append(xmlFilePath).append(newLine);
+                        finalMessage.append(errorBuffer);
+                        break;
+                    }
+
+                    finalMessage.append("Erori la validare fisier; cod eroare=").append(Integer.toString(returnCode)).append(newLine);
+                    break;
+            }
+
+            if (returnCode >= FISIER_VALID) {
+                pdfCreation.createPdf(validator.getInfo(), pdfFilePath, xmlFilePath, "");
+                return new Result(finalMessage.toString(), pdfFilePath, returnCode);
+            }
+
+            return new Result(finalMessage.toString(), "", returnCode);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return new Result(e.getMessage(), "", UNKNOWN_ERROR);
+        }
+    }
+
 }
--- src/test/java/ro/incremental/anaf/declaratii/TestValidators.java
+++ src/test/java/ro/incremental/anaf/declaratii/TestValidators.java
@@ -1,5 +1,6 @@
 package ro.incremental.anaf.declaratii;
 
+import com.google.common.io.Files;
 import dec.Info;
 import dec.LogTrace;
 import dec.Validation;
@@ -9,8 +10,10 @@ import pdf.PdfCreation;
 import pdf.PdfCreatorRoot;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -25,139 +28,23 @@ public class TestValidators {
     @Test
     public void testValidators() throws Exception {
 
-        InputStream packages = Thread.currentThread().getContextClassLoader().getResourceAsStream("packages.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(packages));
-
-        Map<String, Class<PdfCreation>> creators = new HashMap<>();
-        Map<String, Class<Validation>> validators = new HashMap<>();
-
-        String line;
-        while((line = reader.readLine()) != null) {
-            creators.put(line, (Class<PdfCreation>)Class.forName(line + ".PdfCreator"));
-            validators.put(line, (Class<Validation>)Class.forName(line + "validator.Validator"));
-        }
-
-//        for(String key: creators.keySet()) {
-//            System.out.println(key + "->" + creators.get(key).toString());
-//            System.out.println(key + "->" + validators.get(key).toString());
-//            PdfCreatorRoot creator = (PdfCreatorRoot) creators.get(key);
-//        }
-
         String declName = "d106";
 
-//        boolean containsDec = false;
-//        for(Package p : Package.getPackages()) {
-//
-//            containsDec |= p.getName().contains(declName);
-//
-//        }
-
-        Validation validation = validators.get(declName).newInstance();
-        PdfCreation pdfCreation = creators.get(declName).newInstance();
-
-        int code = parseDocumentXML("./examples/declaratie106.xml", validation, pdfCreation, declName.toUpperCase());
-        System.out.println(code);
-
-//        assertTrue("Anaf libraries are in test classpath", containsDec);
-
-    }
-
-    private int parseDocumentXML(String xmlFile, Validation _validator, PdfCreation pdfCreation, String _declType)
-    {
-        int returns;
-        try
-        {
-
-            String errFile = xmlFile + ".err.txt";
-            StringBuffer _finalMessage = new StringBuffer();
-            String _newLine = System.lineSeparator();
-            Info _validationInfo;
-
-            returns = _validator.parseDocument(xmlFile, errFile);
-
-            if(returns < 0)
-            {
-                if(returns > -4)
-                {
-                    _finalMessage.append("Erori la validare fisier: ").append(xmlFile).append(_newLine);
-                    _finalMessage.append("       Erorile au fost scrise in fisierul: ").append(errFile).append(_newLine);
-                }
-                else if(returns == -4)
-                {
-                    _finalMessage.append("Perioada raportare eronata: ").append(_declType).append(_newLine);
-                }
-                else if(returns == -8)
-                {
-                    _finalMessage.append("Tip declaratie necunoscut: ").append(_declType).append(_newLine);
-                }
-                else
-                {
-                    _finalMessage.append("Erori la validare fisier; cod eroare=").append(Integer.toString(returns)).append(_newLine);
-                }
+        File errFile = new File("examples/declaratie106.xml");
+        StringBuilder xmlBuffer = new StringBuilder();
+        if(errFile.exists()) {
+            BufferedReader reader = Files.newReader(errFile, Charset.forName("UTF-8"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                xmlBuffer.append(line).append(System.lineSeparator());
             }
-            else if(returns > 0)
-            {
-                _finalMessage.append("Atentionari la validare fisier: ").append(xmlFile).append(_newLine);
-                _finalMessage.append("       Atentionarile au fost scrise in fisierul: ").append(errFile).append(_newLine);
-            }
-            else
-            {
-                _finalMessage.append("Validare fara erori fisier: ").append(xmlFile).append(_newLine);
-            }
-
-            if(returns >= 0)
-            {
-                _validationInfo = _validator.getInfo();
-
-                pdfCreation.createPdf(_validationInfo, xmlFile+".pdf", xmlFile, "");
-            }
-            else
-            {
-                _validationInfo = null;
-            }
-            System.out.println(_finalMessage);
-
-            return returns;
         }
-        catch(Throwable e)
-        {
-            return -9;
-        }
+
+
+        ValidateWebService.Result result = ValidateWebService.generateFromXMLString(xmlBuffer.toString(), declName);
+
+        System.out.println(result.toString());
     }
 
 
-
-//    private int validateXML(String fileName)
-//    {
-//        int returns;
-//        try
-//        {
-//            returns = _integrator.parseDocument(fileName, null);
-//            txtResults.append(_integrator.getFinalMessage());
-//            return returns;
-//        }
-//        catch(Throwable e)
-//        {
-//            txtResults.append("Eroare de deployment!" + _newLine);
-//            txtResults.append("       mesaj: " + e.toString() + _newLine);
-//            return -2;
-//        }
-//    }
-//
-//    private int pdfCreation(String fileName)
-//    {
-//        int ret = 0;
-//        try
-//        {
-//            ret = _integrator.pdfCreation(fileName, null, null, null);
-//            txtResults.append(_integrator.getFinalMessage());
-//            return ret;
-//        }
-//        catch(Throwable e)
-//        {
-//            txtResults.append("Eroare de deployment!" + _newLine);
-//            txtResults.append("       mesaj: " + e.toString() + _newLine);
-//            return -2;
-//        }
-//    }
 }
